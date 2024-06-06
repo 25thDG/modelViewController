1eb30c84d124558aa13682c0932a23871dcaf4b2
Dldar Gulani <dldar.gulani@stud.th-luebeck.de>
2024-06-06 13:58:28 +0200

    Random Integers and two dimensional array

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import javax.swing.JButton;
+
 public class Main {
 
     public static void main(String[] args) {
+        JButton[][] buttons = new JButton[4][4];
+        System.out.println(buttons.length);
         PuzzleView game = new PuzzleView();
         game.setVisible(true);
     }
--- PuzzleView.java
+++ PuzzleView.java
@@ -7,7 +7,23 @@ public class PuzzleView extends JFrame{
     private static final Font FONT = new Font("Eurostile", Font.BOLD,28);
  
 
-    private List<JButton> buttons = new LinkedList<>();
+    private JButton[][] buttons = new JButton[4][4];
+
+    //Random int generator for the Game
+    private List<Integer> randomIntegers() {
+        List<Integer> rndomIntegers = new ArrayList<>();
+        Random r = new Random();
+        int i = 0;
+        int nextInt = 1;
+        while (i != 15) {
+            nextInt = r.nextInt(1,16);
+            if (!rndomIntegers.contains(nextInt)) {
+                rndomIntegers.add(nextInt);
+                i++;
+            }
+        }
+        return rndomIntegers;
+    }
 
     public PuzzleView() {
         //name of the window
@@ -15,15 +31,26 @@ public class PuzzleView extends JFrame{
         this.setSize(350,400);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanel pieces = new JPanel(new GridLayout(4,4,7,7));
-        for (char c : "abcdefghijklmnop".toUpperCase().toCharArray()) {
-            JButton button = new JButton(c + "");
-            button.setFont(FONT);
-            buttons.add(button);
-            pieces.add(button);
+        int elementsInPieces = 0;
+        List<Integer> randomNumbers = randomIntegers();
+        for (int row = 0; row < buttons.length; row++) {
+            for (int column = 0; column < buttons.length; column++) {
+                buttons[row][column] = new JButton();
+                //check if 15 numbers are in already if so create the empty piece
+                if (elementsInPieces < randomNumbers.size()) {
+                    buttons[row][column].setText(String.valueOf(randomNumbers.get(elementsInPieces)));
+                    pieces.add(buttons[row][column]);
+                    elementsInPieces++;
+                } else {
+                    buttons[row][column].setText("");
+                    pieces.add(buttons[row][column]);
+                }
+            }
         }
+        
         this.add(pieces,BorderLayout.CENTER);
     }
-    public List<JButton> geButtons() {return buttons;}
+    public JButton[][] geButtons() {return buttons;}
 
     //public void show(Puzzle puzzle) {
     //}
